1b76bd4c71e032d862941c0f46a4fecc524e5048
Diane Crame <dcrame@icloud.com>
2020-07-24 11:24:25 -0700

    added validations

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,3 +1,5 @@
 class Hike < ApplicationRecord
     belongs_to :location
+    validates :name, uniqueness: true
+    validates :distance, :difficulty, :elevation_gain, :location_id, :website, presence: true
 end
