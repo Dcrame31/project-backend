80d500c2f8ddb534833befdb5b6dc44fc2694f60
Diane Crame <dcrame@icloud.com>
2020-07-31 10:27:38 -0700

    updated validations in hike model

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,5 @@ class Hike < ApplicationRecord
     belongs_to :location
     validates :name, uniqueness: true
     validates :distance, :difficulty, :elevation_gain, :location_id, :website, presence: true
+    # validates :distance, presence:true, numericality: {only_float: true}
 end
